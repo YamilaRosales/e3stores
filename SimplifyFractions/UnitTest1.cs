58e5f08627ad3cfe9964a25780bfdd38b15814df
Yamila Rosales <Yamila.Rosales@lexicacorp.com>
2022-07-14 16:32:16 -0300

    Initial Version

--- SimplifyFractions/UnitTest1.cs
+++ SimplifyFractions/UnitTest1.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-
+using FraccionesLib;
 
 namespace SimplifyFractionsTest
 {
     [TestClass]
-    public class SimplificarTest    
+    public class SimplificarTest
     {
         [TestMethod]
         public void SimplificarFracciones()
         {
-            Simplificar("4/6");
-            Simplificar("10/11");
-            Simplificar("100/400")
+            var f = new Fracciones();
+            f.Simplificar("4/6");
+            f.Simplificar("10/11");
+            f.Simplificar("100/400");
         }
     }
 }
